331a9d52ea3867723fe0fbf2da53d929a3d6900c
Задорин Артем <artfan95@gmail.com>
2025-02-10 17:14:01 +0700

    скрипт для видео

--- src/main.js
+++ src/main.js
@@ -1,26 +1,27 @@
 import 'normalize.css'
 import './styles/fonts.css'
 import './styles/style.scss'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+const video = document.querySelector('.video');
+let camera_stream = null;
 
-setupCounter(document.querySelector('#counter'))
+start.addEventListener('click', startVideo)
+stop.addEventListener('click', stopVideo)
+
+function startVideo(){
+  console.log('startVideo')
+  navigator.mediaDevices.getUserMedia({video: true})
+  .then( (result) => {
+    camera_stream = result
+    video.srcObject = camera_stream;
+    video.play();
+  } )  
+  .catch( (error) => console.log(error))
+}
+
+function stopVideo(){
+  console.log('stopVideo')
+  camera_stream.getTracks()[0].stop();
+}
